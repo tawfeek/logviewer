b102d94dc53b3f438ae064cef9f14395a62ba36b
Tawfeek Masalha <tawfeeksami@gmail.com>
2019-01-08 19:45:00 +0200

    adding controller

---
+++ src/main/java/com/log/loggitor/web/AppController.java
@@ -0,0 +1,23 @@
+package com.log.loggitor.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.log.loggitor.domain.App;
+import com.log.loggitor.repository.AppRepository;
+
+@RestController
+public class AppController {
+	
+	@Autowired
+    private  AppRepository appRepository;
+
+	@RequestMapping("/app")
+    public Iterable<App> getApps() {
+
+		return appRepository.findAll();
+    } 
+
+
+}
